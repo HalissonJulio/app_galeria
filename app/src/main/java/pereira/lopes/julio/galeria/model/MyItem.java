2298d3dbdb24c1fa375f906f65cacecfd344c406
HalissonJulio <halissonjulio@gmail.com>
2023-04-07 22:48:39 -0300

    Criado novo github por causa de erro de criação, 07/04/23.

---
+++ app/src/main/java/pereira/lopes/julio/galeria/model/MyItem.java
@@ -0,0 +1,9 @@
+package pereira.lopes.julio.galeria.model;
+
+import android.net.Uri;
+
+public class MyItem {
+    public Uri photo; // Guarda o link da img
+    public String title; // Guarda o titulo
+    public String description; // Guarda a descriçao
+}
